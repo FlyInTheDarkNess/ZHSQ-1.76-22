eae19620b260def090d8a273daf48b8e3270e760
FlyInTheDarkNess <916879006@qq.com>
2015-03-19 10:29:12 +0800

    1.修改为3.11接口

--- AXHRevamped/Untils/AXHHttpUrl.h
+++ AXHRevamped/Untils/AXHHttpUrl.h
@@ -9,122 +9,122 @@
 #ifndef ZHSQ_AXHHttpUrl_h
 #define ZHSQ_AXHHttpUrl_h
 ////优惠商品列表主键获取接口
-//#define kYHGOODS_ID_URL  @"http://115.28.149.27:8090/V3.10/WebUI/api/serverApi/m40_01.ashx"
+//#define kYHGOODS_ID_URL  @"http://115.28.149.27:8090/V3.11/WebUI/api/serverApi/m40_01.ashx"
 ////优惠商品列表获取接口
-//#define kYHGOODS_LIST_URL @"http://115.28.149.27:8090/V3.10/WebUI/api/serverApi/m40_02.ashx"
+//#define kYHGOODS_LIST_URL @"http://115.28.149.27:8090/V3.11/WebUI/api/serverApi/m40_02.ashx"
 ////优惠商品简略主键获取接口
-//#define kYHGOODS_BRIEF_ID_URL @"http://115.28.149.27:8090/V3.10/WebUI/api/serverApi/m40_03.ashx"
+//#define kYHGOODS_BRIEF_ID_URL @"http://115.28.149.27:8090/V3.11/WebUI/api/serverApi/m40_03.ashx"
 ////优惠商品简略信息列表获取接口
-//#define kYHGOODS_BRIEF_LIST_URL @"http://115.28.149.27:8090/V3.10/WebUI/api/serverApi/m40_04.ashx"
+//#define kYHGOODS_BRIEF_LIST_URL @"http://115.28.149.27:8090/V3.11/WebUI/api/serverApi/m40_04.ashx"
 ////取得优惠商品信息及对应的多个商家
-//#define kYHGOODS_GOODS_DETAIL_URL @"http://115.28.149.27:8090/V3.10/WebUI/api/serverApi/m40_07.ashx"
+//#define kYHGOODS_GOODS_DETAIL_URL @"http://115.28.149.27:8090/V3.11/WebUI/api/serverApi/m40_07.ashx"
 ////生活帮界面
-//#define kLEFT_TO_HELP_LIST_URL @"http://115.28.149.27:8090/V3.10/WebUI/api/serverApi/m42_01.ashx"
+//#define kLEFT_TO_HELP_LIST_URL @"http://115.28.149.27:8090/V3.11/WebUI/api/serverApi/m42_01.ashx"
 ////秒杀商品列表主键获取接口
-//#define kSECKILL_ID_URL @"http://115.28.149.27:8090/V3.10/WebUI/api/serverApi/m40_05.ashx"
+//#define kSECKILL_ID_URL @"http://115.28.149.27:8090/V3.11/WebUI/api/serverApi/m40_05.ashx"
 //// 秒杀商品列表获取接口
-//#define kSECKILL_LIST_URL @"http://115.28.149.27:8090/V3.10/WebUI/api/serverApi/m40_06.ashx"
+//#define kSECKILL_LIST_URL @"http://115.28.149.27:8090/V3.11/WebUI/api/serverApi/m40_06.ashx"
 ////秒杀商品秒杀接口
-//#define kSECKILL_GOODS_URL @"http://115.28.149.27:8090/V3.10/WebUI/api/serverApi/m40_08.ashx"
+//#define kSECKILL_GOODS_URL @"http://115.28.149.27:8090/V3.11/WebUI/api/serverApi/m40_08.ashx"
 ////秒杀商品详情接口
-//#define kSECKILL_GOODS_DETAIL_URL @"http://115.28.149.27:8090/V3.10/WebUI/api/serverApi/m40_09.ashx"
+//#define kSECKILL_GOODS_DETAIL_URL @"http://115.28.149.27:8090/V3.11/WebUI/api/serverApi/m40_09.ashx"
 ////商家详情接口
-//#define kSTORE_DETAIL_URL @"http://115.28.149.27:8090/V3.10/WebUI/api/serverApi/m3_03.ashx"
+//#define kSTORE_DETAIL_URL @"http://115.28.149.27:8090/V3.11/WebUI/api/serverApi/m3_03.ashx"
 ////报新鲜接口
-//#define kFRESH_INFOR_URL @"http://115.28.149.27:8090/V3.10/WebUI/api/serverApi/m20_02.ashx"
+//#define kFRESH_INFOR_URL @"http://115.28.149.27:8090/V3.11/WebUI/api/serverApi/m20_02.ashx"
 ////报新鲜表单接口
-//#define kFRESH_INFOR_LIST_URL @"http://115.28.149.27:8090/V3.10/WebUI/api/serverApi/m20_03.ashx"
+//#define kFRESH_INFOR_LIST_URL @"http://115.28.149.27:8090/V3.11/WebUI/api/serverApi/m20_03.ashx"
 ////取得登录用户发布信息主键列表
-//#define kUSER_NEWS_URL @"http://115.28.149.27:8090/V3.10/WebUI/api/serverApi/m20_08.ashx"
+//#define kUSER_NEWS_URL @"http://115.28.149.27:8090/V3.11/WebUI/api/serverApi/m20_08.ashx"
 ////收藏信息提交
-//#define kUSER_BUSINESS_URL @"http://115.28.149.27:8090/V3.10/WebUI/api/serverApi/m28_01.ashx"
+//#define kUSER_BUSINESS_URL @"http://115.28.149.27:8090/V3.11/WebUI/api/serverApi/m28_01.ashx"
 ////对资讯赞（踩）一下
-//#define kUSER_NEWS_UPORDOWN_URL @"http://115.28.149.27:8090/V3.10/WebUI/api/serverApi/m38_06.ashx"
+//#define kUSER_NEWS_UPORDOWN_URL @"http://115.28.149.27:8090/V3.11/WebUI/api/serverApi/m38_06.ashx"
 ///**
 // *咨询主键ID接口
 // */
-//#define kSOURCE_NEW_ID_URL @"http://115.28.149.27:8090/V3.10/WebUI/api/serverApi/m38_04.ashx"
+//#define kSOURCE_NEW_ID_URL @"http://115.28.149.27:8090/V3.11/WebUI/api/serverApi/m38_04.ashx"
 ///**
 // *咨询评论列表接口
 // */
-//#define kSOURCE_NEW_LIST_URL @"http://115.28.149.27:8090/V3.10/WebUI/api/serverApi/m38_05.ashx"
+//#define kSOURCE_NEW_LIST_URL @"http://115.28.149.27:8090/V3.11/WebUI/api/serverApi/m38_05.ashx"
 ///**
 // *咨询评论接口
 // */
-//#define kSOURCE_NEW_FEEDBACK_URL @"http://115.28.149.27:8090/V3.10/WebUI/api/serverApi/m38_03.ashx"
+//#define kSOURCE_NEW_FEEDBACK_URL @"http://115.28.149.27:8090/V3.11/WebUI/api/serverApi/m38_03.ashx"
 ////积分商城列表主键接口
-//#define kRULE_ID_URL @"http://115.28.149.27:8090/V3.10/WebUI/api/serverApi/m19_06.ashx"
+//#define kRULE_ID_URL @"http://115.28.149.27:8090/V3.11/WebUI/api/serverApi/m19_06.ashx"
 ////积分商城列表接口
-//#define kRULE_LIST_URL @"http://115.28.149.27:8090/V3.10/WebUI/api/serverApi/m19_07.ashx"
+//#define kRULE_LIST_URL @"http://115.28.149.27:8090/V3.11/WebUI/api/serverApi/m19_07.ashx"
 ///**
 // *报料接口
 // */
-//#define kSOURCE_NEW_URL @"http://115.28.149.27:8090/V3.10/WebUI/api/serverApi/m44_07.aspx"
+//#define kSOURCE_NEW_URL @"http://115.28.149.27:8090/V3.11/WebUI/api/serverApi/m44_07.aspx"
 //
 ///**
 // *爆新鲜接口
 // */
-//#define kBXX_NEW_URL @"http://115.28.149.27:8090/V3.10/WebUI/api/serverApi/m20_07.aspx"
+//#define kBXX_NEW_URL @"http://115.28.149.27:8090/V3.11/WebUI/api/serverApi/m20_07.aspx"
 
 //**************************************************************************************************************
 
 //优惠商品列表主键获取接口
-#define kYHGOODS_ID_URL  @"http://114.215.147.74:8088/V3.10/WebUI/api/serverApi/m40_01.ashx"
+#define kYHGOODS_ID_URL  @"http://114.215.147.74:8088/V3.11/WebUI/api/serverApi/m40_01.ashx"
 //优惠商品列表获取接口
-#define kYHGOODS_LIST_URL @"http://114.215.147.74:8088/V3.10/WebUI/api/serverApi/m40_02.ashx"
+#define kYHGOODS_LIST_URL @"http://114.215.147.74:8088/V3.11/WebUI/api/serverApi/m40_02.ashx"
 //优惠商品简略主键获取接口
-#define kYHGOODS_BRIEF_ID_URL @"http://114.215.147.74:8088/V3.10/WebUI/api/serverApi/m40_03.ashx"
+#define kYHGOODS_BRIEF_ID_URL @"http://114.215.147.74:8088/V3.11/WebUI/api/serverApi/m40_03.ashx"
 //优惠商品简略信息列表获取接口
-#define kYHGOODS_BRIEF_LIST_URL @"http://114.215.147.74:8088/V3.10/WebUI/api/serverApi/m40_04.ashx"
+#define kYHGOODS_BRIEF_LIST_URL @"http://114.215.147.74:8088/V3.11/WebUI/api/serverApi/m40_04.ashx"
 //取得优惠商品信息及对应的多个商家
-#define kYHGOODS_GOODS_DETAIL_URL @"http://114.215.147.74:8088/V3.10/WebUI/api/serverApi/m40_07.ashx"
+#define kYHGOODS_GOODS_DETAIL_URL @"http://114.215.147.74:8088/V3.11/WebUI/api/serverApi/m40_07.ashx"
 //生活帮界面
-#define kLEFT_TO_HELP_LIST_URL @"http://114.215.147.74:8088/V3.10/WebUI/api/serverApi/m42_01.ashx"
+#define kLEFT_TO_HELP_LIST_URL @"http://114.215.147.74:8088/V3.11/WebUI/api/serverApi/m42_01.ashx"
 //秒杀商品列表主键获取接口
-#define kSECKILL_ID_URL @"http://114.215.147.74:8088/V3.10/WebUI/api/serverApi/m40_05.ashx"
+#define kSECKILL_ID_URL @"http://114.215.147.74:8088/V3.11/WebUI/api/serverApi/m40_05.ashx"
 // 秒杀商品列表获取接口
-#define kSECKILL_LIST_URL @"http://114.215.147.74:8088/V3.10/WebUI/api/serverApi/m40_06.ashx"
+#define kSECKILL_LIST_URL @"http://114.215.147.74:8088/V3.11/WebUI/api/serverApi/m40_06.ashx"
 //秒杀商品秒杀接口
-#define kSECKILL_GOODS_URL @"http://114.215.147.74:8088/V3.10/WebUI/api/serverApi/m40_08.ashx"
+#define kSECKILL_GOODS_URL @"http://114.215.147.74:8088/V3.11/WebUI/api/serverApi/m40_08.ashx"
 //秒杀商品详情接口
-#define kSECKILL_GOODS_DETAIL_URL @"http://114.215.147.74:8088/V3.10/WebUI/api/serverApi/m40_09.ashx"
+#define kSECKILL_GOODS_DETAIL_URL @"http://114.215.147.74:8088/V3.11/WebUI/api/serverApi/m40_09.ashx"
 //商家详情接口
-#define kSTORE_DETAIL_URL @"http://114.215.147.74:8088/V3.10/WebUI/api/serverApi/m3_03.ashx"
+#define kSTORE_DETAIL_URL @"http://114.215.147.74:8088/V3.11/WebUI/api/serverApi/m3_03.ashx"
 //报新鲜接口
-#define kFRESH_INFOR_URL @"http://114.215.147.74:8088/V3.10/WebUI/api/serverApi/m20_02.ashx"
+#define kFRESH_INFOR_URL @"http://114.215.147.74:8088/V3.11/WebUI/api/serverApi/m20_02.ashx"
 //报新鲜表单接口
-#define kFRESH_INFOR_LIST_URL @"http://114.215.147.74:8088/V3.10/WebUI/api/serverApi/m20_03.ashx"
+#define kFRESH_INFOR_LIST_URL @"http://114.215.147.74:8088/V3.11/WebUI/api/serverApi/m20_03.ashx"
 //取得登录用户发布信息主键列表
-#define kUSER_NEWS_URL @"http://114.215.147.74:8088/V3.10/WebUI/api/serverApi/m20_08.ashx"
+#define kUSER_NEWS_URL @"http://114.215.147.74:8088/V3.11/WebUI/api/serverApi/m20_08.ashx"
 //收藏信息提交
-#define kUSER_BUSINESS_URL @"http://114.215.147.74:8088/V3.10/WebUI/api/serverApi/m28_01.ashx"
+#define kUSER_BUSINESS_URL @"http://114.215.147.74:8088/V3.11/WebUI/api/serverApi/m28_01.ashx"
 //对资讯赞（踩）一下
-#define kUSER_NEWS_UPORDOWN_URL @"http://114.215.147.74:8088/V3.10/WebUI/api/serverApi/m38_06.ashx"
+#define kUSER_NEWS_UPORDOWN_URL @"http://114.215.147.74:8088/V3.11/WebUI/api/serverApi/m38_06.ashx"
 /**
  *咨询主键ID接口
  */
-#define kSOURCE_NEW_ID_URL @"http://114.215.147.74:8088/V3.10/WebUI/api/serverApi/m38_04.ashx"
+#define kSOURCE_NEW_ID_URL @"http://114.215.147.74:8088/V3.11/WebUI/api/serverApi/m38_04.ashx"
 /**
 *咨询评论列表接口
 */
-#define kSOURCE_NEW_LIST_URL @"http://114.215.147.74:8088/V3.10/WebUI/api/serverApi/m38_05.ashx"
+#define kSOURCE_NEW_LIST_URL @"http://114.215.147.74:8088/V3.11/WebUI/api/serverApi/m38_05.ashx"
 /**
  *咨询评论接口
  */
-#define kSOURCE_NEW_FEEDBACK_URL @"http://114.215.147.74:8088/V3.10/WebUI/api/serverApi/m38_03.ashx"
+#define kSOURCE_NEW_FEEDBACK_URL @"http://114.215.147.74:8088/V3.11/WebUI/api/serverApi/m38_03.ashx"
 //积分商城列表主键接口
-#define kRULE_ID_URL @"http://114.215.147.74:8088/V3.10/WebUI/api/serverApi/m19_06.ashx"
+#define kRULE_ID_URL @"http://114.215.147.74:8088/V3.11/WebUI/api/serverApi/m19_06.ashx"
 //积分商城列表接口
-#define kRULE_LIST_URL @"http://114.215.147.74:8088/V3.10/WebUI/api/serverApi/m19_07.ashx"
+#define kRULE_LIST_URL @"http://114.215.147.74:8088/V3.11/WebUI/api/serverApi/m19_07.ashx"
 /**
  *报料接口
  */
-#define kSOURCE_NEW_URL @"http://114.215.147.74:8088/V3.10/WebUI/api/serverApi/m44_07.aspx"
+#define kSOURCE_NEW_URL @"http://114.215.147.74:8088/V3.11/WebUI/api/serverApi/m44_07.aspx"
 
 /**
  *爆新鲜接口
  */
-#define kBXX_NEW_URL @"http://114.215.147.74:8088/V3.10/WebUI/api/serverApi/m20_07.aspx"
+#define kBXX_NEW_URL @"http://114.215.147.74:8088/V3.11/WebUI/api/serverApi/m20_07.aspx"
 
 
 /**
